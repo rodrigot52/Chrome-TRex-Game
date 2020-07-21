00d5101871ba4478a4ec412d44f6685244eaca14
Rodrigo Teixeira <rodrigoteixeira52@hotmail.com>
2020-07-21 15:46:38 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,77 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dino = document.querySelector('.dino')
+    const grid = document.querySelector('.grid')
+    const alert = document.getElementById('alert')
+    let isJumping = false
+    let gravity = 0.9
+    let isGameOVer = false
+
+    function control(e) {
+        if(e.keyCode === 32) {
+            if (!isJumping) {
+                isJumping = true
+                jump()
+            }
+        }
+    }
+    document.addEventListener('keyup', control)
+
+    let position = 0
+    function jump() {
+        let count = 0
+        let timerId = setInterval(function () {
+            
+            // Move Down
+            if (count === 15) {
+                clearInterval(timerId)
+                let downTimerId = setInterval(function () {
+                    if (count === 0) {
+                        clearInterval(downTimerId)
+                        isJumping = false
+                    }
+                    position -= 5
+                    count --
+                    position = position * gravity
+                    dino.style.bottom = position + 'px'
+                }, 20)
+            }
+            
+            // Move Up
+            position +=30
+            count ++
+            position = position * gravity
+            dino.style.bottom = position + 'px'
+        }, 20)
+    }
+
+    function generateObstacles() {
+        let randomTime = Math.random() * 4000
+        let obstaclePosition = 1000
+        const obstacle = document.createElement('div')
+        if(!isGameOVer) obstacle.classList.add('obstacle')
+        grid.appendChild(obstacle)
+        obstacle.style.left = obstaclePosition + 'px'
+
+        let timerId = setInterval(function() {
+            if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
+                clearInterval(timerId)
+                alert.innerHTML = 'Game Over'
+                isGameOVer = true
+
+                // Remove all children
+
+                while (grid.firstChild) {
+                    grid.removeChild(grid.lastChild)
+                }
+            }
+
+            obstaclePosition -= 10
+            obstacle.style.left = obstaclePosition + 'px'
+        }, 20)
+        
+        if(!isGameOVer) setTimeout(generateObstacles, randomTime)
+
+    }
+    generateObstacles()
+
+})
